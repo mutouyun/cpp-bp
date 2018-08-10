767e4a5ee5c18152d496622ef3aacae8ab7a11eb
mutouyun <mark.lonr@tom.com>
2018-08-10 18:30:23 +0800

    添加测试

--- main.cpp
+++ main.cpp
@@ -1,21 +1,40 @@
 #include "functions.hpp"
 
-int main()
+int main(void)
 {
     using namespace BP;
 
-    matrix_t<4, 4> X
+//    matrix_t<4, 4> X
+//    {
+//        vector(5.1, 3.5, 1.4, 0.2),
+//        vector(4.9, 3.0, 1.4, 0.2),
+//        vector(6.2, 3.4, 5.4, 2.3),
+//        vector(5.9, 3.0, 5.1, 1.8)
+//    };
+//    auto Y = vector(0, 0, 1, 1);
+//    auto W = vector(0.5, 0.5, 0.5, 0.5);
+
+//    for (int i = 0; i < 1000; ++i)
+//    {
+//        auto P = sigmoid(X * W);
+//        std::cout << (i + 1) << ":\tP = " << P << ",\tW = " << W << std::endl;
+//        auto P_error = Y - P;
+//        auto P_delta = P_error * sigmoid_d(P);
+//        auto W_delta = transpose(X) * P_delta;
+//        W = W + W_delta;
+//    }
+
+    matrix_t<4, 2> X
     {
-        vector(5.1, 3.5, 1.4, 0.2),
-        vector(4.9, 3.0, 1.4, 0.2),
-        vector(6.2, 3.4, 5.4, 2.3),
-        vector(5.9, 3.0, 5.1, 1.8)
+        vector(0.1, 1),
+        vector(0  , 0.8),
+        vector(0.7, 1),
+        vector(1.1, 0.8)
     };
-
+    auto W = vector(0.5, 0.5);
     auto Y = vector(0, 0, 1, 1);
-    auto W = vector(0.5, 0.5, 0.5, 0.5);
 
-    for (int i = 0; i < 100; ++i)
+    for (int i = 0; i < 1000; ++i)
     {
         auto P = sigmoid(X * W);
         std::cout << (i + 1) << ":\tP = " << P << ",\tW = " << W << std::endl;
@@ -24,5 +43,12 @@ int main()
         auto W_delta = transpose(X) * P_delta;
         W = W + W_delta;
     }
+
+    std::cout << "Test(0.2, 0.9):\tP = " << sigmoid(matrix_t<1, 2>{ vector(0.2, 0.9) } * W) << std::endl;
+    std::cout << "Test(0.1, 1  ):\tP = " << sigmoid(matrix_t<1, 2>{ vector(0.1, 1  ) } * W) << std::endl;
+    std::cout << "Test(0  , 1  ):\tP = " << sigmoid(matrix_t<1, 2>{ vector(0  , 1  ) } * W) << std::endl;
+    std::cout << "Test(0.8, 0.9):\tP = " << sigmoid(matrix_t<1, 2>{ vector(0.8, 0.9) } * W) << std::endl;
+    std::cout << "Test(0.9, 1  ):\tP = " << sigmoid(matrix_t<1, 2>{ vector(0.9, 1  ) } * W) << std::endl;
+    std::cout << "Test(1  , 1  ):\tP = " << sigmoid(matrix_t<1, 2>{ vector(1  , 1  ) } * W) << std::endl;
     return 0;
 }
